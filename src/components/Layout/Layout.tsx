cc01eea9932ca4fd5778dd3ce422f10e7422a852
AriaTofighi <aria.tofighi1@gmail.com>
2026-03-27 02:44:39 -0700

    Fix theming issue

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,21 +5,27 @@ import { Sidebar } from "./Sidebar";
 import { Header } from "./Header";
 import { useKeyboardShortcuts } from "@/hooks/useKeyboardShortcuts";
 
-export function Layout() {
-  // Activate global keyboard shortcuts
+function LayoutContent() {
+  // Activate global keyboard shortcuts (requires SidebarProvider context)
   useKeyboardShortcuts();
 
   return (
-    <SidebarProvider>
-      <div className="flex min-h-screen w-full bg-background text-foreground">
-        <Sidebar />
-        <div className="flex flex-col flex-1 w-full overflow-hidden">
-          <Header />
-          <main className="flex-1 overflow-auto p-4 md:p-6 lg:p-8">
-            <Outlet />
-          </main>
-        </div>
+    <div className="flex min-h-screen w-full bg-background text-foreground">
+      <Sidebar />
+      <div className="flex flex-col flex-1 w-full overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-auto p-4 md:p-6 lg:p-8">
+          <Outlet />
+        </main>
       </div>
+    </div>
+  );
+}
+
+export function Layout() {
+  return (
+    <SidebarProvider>
+      <LayoutContent />
       <Toaster />
     </SidebarProvider>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "next-themes";
 import App from "./App";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
